ef982b1718c4b2c45535fe3079d7b998b7a6883b
WaranyooChuathon <waranyoo.chuathon@gmail.com>
2025-07-10 16:17:52 +0700

    Add Componect header/icon/img

---
+++ src/components/Icon.tsx
@@ -0,0 +1,18 @@
+
+import './Icon.css';
+export function Icon() {
+    return (
+        <div className= 'blue'>
+abc
+        </div>
+
+    );
+}
+
+export function IconExample() {
+    return (
+        <div className='blue'>
+ab
+        </div>
+    );
+}
---
+++ src/components/Image.tsx
@@ -0,0 +1,9 @@
+export default function Image({imageUrl}: {imageUrl: string}) {
+    return (
+        <>
+        
+        <img src={imageUrl} width = "100px"/>
+        </>
+
+    );
+}
