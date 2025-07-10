8313146c97be08cfb9d83e009f4bad00d8028546
WaranyooChuathon <waranyoo.chuathon@gmail.com>
2025-07-10 17:16:22 +0700

    Test react hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import Checkbox from "./components/Checkbox";
 
+import {useState} from "react";
+
+
 function App() {
   const todolist = [
     { text: "coding react", isChecked: false },
@@ -7,14 +10,29 @@ function App() {
     { text: "testing react", isChecked: false },
   ];
 
+ // State to manage the count of button clicks
+  const [count, setCount] = useState(0);
+
+  // Function to handle button click
+  function buttonClick() {
+    setCount(count + 1);
+    console.log("Button clicked", count);
+  }
+
   return (
+
     <div>
+      <div>
+        <h1>To-Do List</h1>
+        <button onClick={buttonClick}>Click me {count}</button> 
+      </div>
       {todolist.map((todo, index) => {
         return (
           <Checkbox key={index} text={todo.text} isChecked={todo.isChecked} />
         );
       })}
     </div>
+
   );
 }
 
